9e334ef5da4cd2044ba5b56492605e6556577365
unknown <pedro.henrickcs@gmail.com>
2021-10-02 16:42:28 -0300

    :rocket: deploy

--- js/modules/load.js
+++
@@ -1,7 +0,0 @@
-'use-strict';
-
-const loadPage = () => {
-  $('body').prepend('<div class="load"><div></div><div></div></div>')
-}
-
-module.exports = loadPage;
--- js/utils/url.js
+++
@@ -1,1 +0,0 @@
-export const URL = 'https://challenge-fielo.herokuapp.com';
